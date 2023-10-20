d9fb97ba3dafb034bbd1a84cd033ffbf68fc83eb
Shreejan Acharya <shreejan.acharya@cnspartner.com>
2023-10-20 19:58:15 -0400

    comments added

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ import models.Triangle;
 import utils.TriangleUtils;
 
 public class Main {
-    
+
     public static void main(String[] args) {
         Triangle[] triangles = new Triangle[3];
         TriangleUtils utils = new TriangleUtils();
-        utils.fillTrianglesArray(triangles);
+        utils.performTaskOnTriangle(triangles);
     }
 }
--- src/utils/TriangleUtils.java
+++ src/utils/TriangleUtils.java
@@ -10,6 +10,12 @@ import java.util.Scanner;
 
 public class TriangleUtils {
 
+    /**
+     * this method return double data enter by the user
+     *
+     * @param message:String Message to display while getting user input
+     * @return double: returns user entered value
+     */
     private double getUserInput(String message) {
         boolean validInput = false;
         double userInput = 0;
@@ -28,7 +34,12 @@ public class TriangleUtils {
         return userInput;
     }
 
-
+    /**
+     * This method create a new vertex taking two input from the user
+     *
+     * @param message: String message to display to user
+     * @return Vertex construct vertex from getting x,y from input
+     */
     private Vertex getTriagleSingleVertex(String message) {
         System.out.println(message);
         double x = getUserInput("Please enter x:");
@@ -36,6 +47,12 @@ public class TriangleUtils {
         return new Vertex(x, y);
     }
 
+    /**
+     * This method construct Triangle by getting 3 vertex input from the user
+     *
+     * @param message: String message to display to user
+     * @return: Triangle it construct the triangle
+     */
     private Triangle getTriangle(String message) {
         System.out.println(message);
         Vertex[] vertices = new Vertex[3];
@@ -45,7 +62,13 @@ public class TriangleUtils {
         return new Triangle(vertices);
     }
 
-    public void fillTrianglesArray(Triangle[] triangles) {
+
+    /**
+     * This method fills and perform task on triangle
+     *
+     * @param triangles: Triangle[] getting user input and calculating perimeter etc
+     */
+    public void performTaskOnTriangle(Triangle[] triangles) {
         for (int i = 0; i < triangles.length; i++) {
             Triangle triangle = getTriangle("Vertices for triangle " + (i + 1) + " :");
             triangles[i] = triangle;
@@ -54,6 +77,11 @@ public class TriangleUtils {
         }
     }
 
+    /**
+     * This method calculates the perimeter and checks if the triangle is isosceles
+     *
+     * @param triangle: Triangle
+     */
     private void calculatePerimeterAndCheckIsosceles(Triangle triangle) {
         Map<Double, Double> distanceMap = new HashMap<>();
         int numberOfVertices = 3;
@@ -81,6 +109,11 @@ public class TriangleUtils {
 
     }
 
+    /**
+     * This method get new point from the user and check if the triangle is inside
+     *
+     * @param traTriangle: Triangle
+     */
     public void checkIfAPointIsInsideTriangle(Triangle traTriangle) {
         Vertex vertex = getTriagleSingleVertex("Please enter a point:");
         Vertex[] triangleVertices = traTriangle.getVertices();
@@ -90,14 +123,28 @@ public class TriangleUtils {
         double area3 = calculateTriangleArea(triangleVertices[0], triangleVertices[1], vertex);
         boolean isInside = triangleArea == (area1 + area2 + area3);
         System.out.printf(isInside ?
-    String.format("%s is inside of the triangle!!!\n",vertex.toString())
-    : "%s is not inside of the triangle!!!\n",vertex.toString());
+                String.format("%s is inside of the triangle!!!\n", vertex.toString())
+                : "%s is not inside of the triangle!!!\n", vertex.toString());
     }
 
+    /**
+     * this method calculates distance between two points
+     *
+     * @param vertex1: Vertex vertex 1 to calculate
+     * @param vertex2: Vertex Vertex 2 to calculate
+     * @return return distance between vertex1 and vertex2
+     */
     private double getDistanceBetweenTwoPoints(Vertex vertex1, Vertex vertex2) {
         return Math.sqrt(Math.pow((vertex1.getX() - vertex2.getX()), 2) + Math.pow((vertex1.getY() - vertex2.getY()), 2));
     }
 
+
+    /**
+     * This method calculates the triangle area
+     *
+     * @param vertices Vertex[]
+     * @return area
+     */
     private double calculateTriangleArea(Vertex... vertices) {
         if (vertices.length != 3) {
             throw new IllegalArgumentException("to calculate area of triangle there need 3 vertices");
@@ -115,6 +162,17 @@ public class TriangleUtils {
         );
     }
 
+    /**
+     * This method calculate from 3 vertex x and y
+     *
+     * @param x1: vertex 1 x
+     * @param y1: vertex 1 y
+     * @param x2: vertex 2 x
+     * @param y2: vertex 2 y
+     * @param x3: vertex 3 x
+     * @param y3: vertex 3 y
+     * @return
+     */
     private double calculateTriangleArea(double x1, double y1, double x2, double y2,
                                          double x3, double y3) {
         return Math.abs((x1 * (y2 - y3) + x2 * (y3 - y1) +
